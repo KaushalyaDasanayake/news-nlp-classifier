d05a0b48563b080e0eceae7b92e5fda9f711d83a
Kaushalya Rathnayake <kaushalyadasanayake97@gmail.com>
2026-02-24 23:28:08 +0530

    add logging file

---
+++ src/newsclf/utils/logging.py
@@ -0,0 +1,15 @@
+# src/newsclf/utils/logging.py
+import logging
+
+
+def get_logger(name: str = "newsclf") -> logging.Logger:
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
+    return logger
